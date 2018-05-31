ada6940bba1cca7320edcf4cef81bf1f4212b341
Robert Martinez <roshi@roberts-mbp.attlocal.net>
2018-05-30 23:50:15 -0700

    added get_items and refactored get_locations to include more information under source hash

--- app/V1/dao.py
+++ app/V1/dao.py
@@ -1,5 +1,5 @@
 from app import db
-from app.V1.models import User, Location
+from app.V1.models import User, Location,Item
 import json
 from sqlalchemy.sql import exists
 from locationservices import LocationService
@@ -27,6 +27,8 @@ def create_user(data):
 def create_item(data):
     pass 
 
+def get_items(data):
+    return Item.query.all()
 
 def get_user(data):
     pass
@@ -36,20 +38,34 @@ def create_location(data):
 
 def get_location(data):
     locs = db.session.query(Location).all()
-    locs_list = [loc.address for loc in locs]
+    locs_list = [loc.address+" "+loc.city+", "+loc.state+", " +str(loc.zipcode) for loc in locs]
     loc_service = LocationService(data.get('source'),locs_list)
 
     res = loc_service.get_addr_by_radius(data.get('radius'))
-    print(res)
+    # print(res)
     results = {"locations":[]}
     for destinations in res:
-        for (source,dest), dist in destinations.items(): 
-            temp = {}
-            temp['source'] = dest
-            temp['distance'] = dist
-            temp['id'] = db.session.query(Location).filter_by(address=dest).first().id
-            #temp[dest] = dist
-            results['locations'].append(temp)
-    print(results)
+        temp = {}
+        for i,(k,v) in enumerate(destinations.items()):
+            if k == "destination":
+                addr = {}
+                addr['address'] = v.get("fulladdress")
+                addr['city'] = v.get("city")
+                addr['state'] = v.get("state")
+                addr['zipcode'] = v.get("zipcode")
+                if not temp:
+                    temp = {}
+                temp['source']=addr
+                temp['id'] = db.session.query(Location).filter_by(address=v.get("fulladdress")).first().id
+
+            else:
+                print("tuple action: ", v)
+                if not temp:
+                    temp = {}
+                temp['distance'] = v
+            if (i+1)%2 == 0:
+                results['locations'].append(temp)
+                temp = {}
+    # print(results)
     return results
 
