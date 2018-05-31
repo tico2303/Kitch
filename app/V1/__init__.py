bfe9b21d9ca7cc13f79e5f5e698f5113ccb2ef6c
Robert Martinez <roshi@roberts-mbp.attlocal.net>
2018-05-30 17:21:14 -0700

    decoupled api models to api_models module

--- app/V1/__init__.py
+++ app/V1/__init__.py
@@ -1,41 +1,30 @@
 from flask import jsonify, Blueprint
-from flask_restplus import Resource, Api, fields, reqparse
+from flask import jsonify
+from flask_restplus import Resource, Api, reqparse
+# from flask_restplus import Resource, fields, reqparse
 
-from app.V1.users_dao import create_user 
-from app.V1.models import User
-from .api_models import *
+from app.V1.dao import create_user,get_location 
+from app.V1.models import User,Location
+from locationservices import LocationService
+from api_models import ApiModel
 
 v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 api = Api(v1, version='1.0', title="Kitch API", description="Documentation for Kitch RESTful API version 1.0")
 
-user_list_format = api.model('User list format',{
-                                "id":fields.String(description="id of User", required=True),
-                                "email":fields.String(description="user's email", required=True),
-                                "fname":fields.String(description="first name ", required=False),
-                                "lname":fields.String(description="last name", required=False)
-                                })
-
-user_list_model = api.model('List of Users', {
-    "user": fields.Nested(user_list_format)
-    })
-
-item_list_format = api.model('Item List format',{
-                                "id":fields.String(description="id of Item", required=True),
-                                "seller_id":fields.String(description="id of Item", required=True),
-
+apimodel = ApiModel(api)
 
 @api.route('/users')
 class UsersList(Resource):
 
-        @api.response(200, 'Success', user_list_model)
+        @api.response(200, 'Success', apimodel.user_list_model())
         # api.doc defines parameters that can be entered in localhost interface
         @api.doc(params={
                 'id':'User Id',
                 'email':'Email address',
-                'fname':'Firstno.',
+                'fname':'First Name',
                 'lname':"Last "})
         # ensures that the format of the get request follows the model
-        @api.marshal_with(user_list_format)
+        @api.marshal_with(apimodel.user_list_format())
         def get(self):
             print("GET called on users endpoint")
             parser = reqparse.RequestParser()
@@ -46,12 +35,10 @@ class UsersList(Resource):
             if args['id'] is None:
                 return {'ValueError':'Invalid userId'},400
 
-            #userlist = user_dao.get_user_list(args['userid'])
-            # userlist = {"userid":"1234","email":"user1@gmail.com","fname":"Greatest","lname":"Ever"}
             userlist = User.query.all()
             return userlist,200
 
-        @api.expect(user_list_model)
+        @api.expect(apimodel.user_list_model())
         def post(self):
             data = api.payload['user']
             print("[+] ", data)
@@ -59,13 +46,34 @@ class UsersList(Resource):
             return newuser
 
 
-@api.route('/item/<int:id>')
-class ItemsList(Resource):
-    
-
+@api.route('/locations')
+class LocationList(Resource):
+    @api.response(200,"Success",apimodel.locations_list())
+    @api.marshal_with(apimodel.locations_list(),envelope="results")
     def get(self):
-        pass
+        locations = Location.query.all()
+        print(locations)
+        return locations,200
 
-    def post(self):
-        pass
+@api.route('/location/radius')
+class LocationList(Resource):
+    @api.response(200, 'Success', apimodel.location_radius_response_list())
+    @api.doc(params={"source":"Source addresss" ,
+                     "radius":"Radius from source in miles"
+                     })
+    @api.marshal_with(apimodel.location_radius_response_format(),envelope="results")
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('source', type=str)
+        parser.add_argument('radius', type=int)
+        parser.add_argument('id', type=int)
+        args = parser.parse_args()
+        source_addr = args['source']
+        radius = args['radius']
+        # print("source_addr: ", source_addr)
+        # print("radius: ", radius)
+        return get_location(args)['locations']
+
+
+
 
